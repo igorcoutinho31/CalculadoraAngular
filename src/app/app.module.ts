2343dfb4243b18dfc10e01fa872786fda8d726a3
Igor Leite Coutinho <igorcoutinho3104@gmail.com>
2025-10-14 11:40:17 -0300

    primeiro commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CalculadoraComponent } from './components/calculadora/calculadora.component';
+import { ResultadoComponent } from './components/resultado/resultado.component';
+import { HistoricoComponent } from './components/historico/historico.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CalculadoraComponent,
+    ResultadoComponent,
+    HistoricoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
